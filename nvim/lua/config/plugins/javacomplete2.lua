ed4dcb8b17c2761908ddc2817c9c87d386203fb0
yimingwangdell <yiming.wang@dell.com>
2023-09-18 19:33:49 +0800

    update orgmode plugin config

--- nvim/lua/config/plugins/javacomplete2.lua
+++ nvim/lua/config/plugins/javacomplete2.lua
@@ -17,14 +17,14 @@ nmap <leader>jR <Plug>(JavaComplete-Imports-RemoveUnused)
 nmap <leader>ji <Plug>(JavaComplete-Imports-AddSmart)
 nmap <leader>jii <Plug>(JavaComplete-Imports-Add)
 
-imap <C-j>I <Plug>(JavaComplete-Imports-AddMissing)
-imap <C-j>R <Plug>(JavaComplete-Imports-RemoveUnused)
-imap <C-j>i <Plug>(JavaComplete-Imports-AddSmart)
-imap <C-j>ii <Plug>(JavaComplete-Imports-Add)
+" imap <C-j>I <Plug>(JavaComplete-Imports-AddMissing)
+" imap <C-j>R <Plug>(JavaComplete-Imports-RemoveUnused)
+" imap <C-j>i <Plug>(JavaComplete-Imports-AddSmart)
+" imap <C-j>ii <Plug>(JavaComplete-Imports-Add)
 
 nmap <leader>jM <Plug>(JavaComplete-Generate-AbstractMethods)
 
-imap <C-j>jM <Plug>(JavaComplete-Generate-AbstractMethods)
+" imap <C-j>jM <Plug>(JavaComplete-Generate-AbstractMethods)
 
 nmap <leader>jA <Plug>(JavaComplete-Generate-Accessors)
 nmap <leader>js <Plug>(JavaComplete-Generate-AccessorSetter)
@@ -35,9 +35,9 @@ nmap <leader>jeq <Plug>(JavaComplete-Generate-EqualsAndHashCode)
 nmap <leader>jc <Plug>(JavaComplete-Generate-Constructor)
 nmap <leader>jcc <Plug>(JavaComplete-Generate-DefaultConstructor)
 
-imap <C-j>s <Plug>(JavaComplete-Generate-AccessorSetter)
-imap <C-j>g <Plug>(JavaComplete-Generate-AccessorGetter)
-imap <C-j>a <Plug>(JavaComplete-Generate-AccessorSetterGetter)
+" imap <C-j>s <Plug>(JavaComplete-Generate-AccessorSetter)
+" imap <C-j>g <Plug>(JavaComplete-Generate-AccessorGetter)
+" imap <C-j>a <Plug>(JavaComplete-Generate-AccessorSetterGetter)
 
 vmap <leader>js <Plug>(JavaComplete-Generate-AccessorSetter)
 vmap <leader>jg <Plug>(JavaComplete-Generate-AccessorGetter)
--- nvim/lua/config/plugins/orgmode.lua
+++ nvim/lua/config/plugins/orgmode.lua
@@ -1,8 +1,56 @@
 return {
-	"dhruvasagar/vim-dotoo",
-	config = function()
-		vim.cmd([[
-                let g:dotoo#agenda_views#agenda#span = 'week'
-	]])
-	end
+	{
+		'nvim-orgmode/orgmode',
+		dependencies = {
+			{ 'nvim-treesitter/nvim-treesitter', lazy = true },
+		},
+		event = 'VeryLazy',
+		config = function()
+			-- Load treesitter grammar for org
+			require('orgmode').setup_ts_grammar()
+
+			-- Setup treesitter
+			require('nvim-treesitter.configs').setup({
+				highlight = {
+					enable = true,
+					additional_vim_regex_highlighting = { 'org' },
+				},
+				ensure_installed = { 'org' },
+			})
+
+			-- Setup orgmode
+			require('orgmode').setup({
+				org_agenda_files = '~/orgfiles/**/*',
+				org_default_notes_file = '~/orgfiles/refile.org',
+			})
+			local cmp = require 'cmp'
+			local has_words_before = function()
+				local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+				return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+			end
+			require('cmp').setup({
+				sources = {
+					{ name = 'orgmode' }
+				},
+				mapping = cmp.mapping.preset.insert({
+					['<C-b>'] = cmp.mapping.scroll_docs(-4),
+					['<C-f>'] = cmp.mapping.scroll_docs(4),
+					['<C-Space>'] = cmp.mapping.complete(),
+					['<C-e>'] = cmp.mapping.abort(),
+					["<Tab>"] = cmp.mapping({
+						i = function(fallback)
+							if cmp.visible() then
+								cmp.select_next_item({ behavior = cmp.SelectBehavior.Insert })
+							elseif has_words_before() then
+								cmp.complete()
+							else
+								fallback()
+							end
+						end,
+					}),
+					['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+				}),
+			})
+		end,
+	}
 }
