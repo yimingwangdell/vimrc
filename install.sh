b384536a591da3153a5094d0220bbcf4910cc3b1
Yiming, Wang <yiming.wang@dell.com>
2025-11-06 16:47:24 +0800

    update

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@ mkdir -p ~/.config/nvim
 mkdir -p ~/.tmux/plugins
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 cp ./nvim/init.vim ~/.config/nvim/init.vim
-cp ./.tmux.conf ~/
+cp ./tmux/.tmux.conf ~/
 sh ./ohmyzsh
 sh ./fzf
