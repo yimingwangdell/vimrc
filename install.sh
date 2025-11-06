245f3ecb9bd10ff5bb750e88aedf71a7347eac77
Yiming, Wang <yiming.wang@dell.com>
2025-11-06 16:54:50 +0800

    update

--- install.sh
+++ install.sh
@@ -2,6 +2,15 @@ mkdir -p ~/.config/nvim
 mkdir -p ~/.tmux/plugins
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 cp ./nvim/init.vim ~/.config/nvim/init.vim
-cp ./tmux/.tmux.conf ~/
+cp .tmux.conf ~/
 sh ./ohmyzsh
 sh ./fzf
+
+cat << 'EOF' >> ~/.zshrc
+alias ra='. ranger'
+alias lg='lazygit'
+alias n='nvim'
+alias ns='nvim `fzf`'
+alias s='vim `fzf`'
+git config --global credential.helper store
+EOF
